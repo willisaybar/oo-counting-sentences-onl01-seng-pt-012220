4ca32f82d1648aed2fea0627d768c62e33c055ee
Willis Aybar <willisaybar@gmail.com>
2020-02-11 02:49:25 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
+
+#return true if ends in a period
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
 end
