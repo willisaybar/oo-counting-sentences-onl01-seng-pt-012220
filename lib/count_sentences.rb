ba1a15c3953c5daf99ddde0bb6c42d74f469fdd3
Willis Aybar <willisaybar@gmail.com>
2020-02-11 02:22:28 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,8 +4,12 @@ class String
 
 
 #return true if ends in a period
-  def sentence?
-    self.sentence? == sentence?.end_with?(".")
+  def sentence?(ends_with)
+    self.sentence?
+    @ends_with = ends_with
+    if @ends_with == sentence?.end_with?(".")
+      puts end_with
+    end
   end
 
   def question?
